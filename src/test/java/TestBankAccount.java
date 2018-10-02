1fccf848db03b5abe461dd8dc269f9a4f8e7ecee
awsdora <34741176+awsdora@users.noreply.github.com>
2018-10-02 15:35:40 +0530

    added test code

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 
 public class TestBankAccount {
     @Test
-    public void testFunds() {
+public void testFunds() {
+        System.out.println("Account intial balance: 10Rs");
        BankAccount account = new BankAccount(10);
+       System.out.println("Debitted amount:" + 5.0);
        double amount = account.debit(5);
-       Assert.assertEquals(5.0, amount);
+       System.out.println("Balance : " +amount);
+       System.out.println("Depositing : " + 100.0);
+       double balance = account.deposit(100);
+       System.out.println("Balance: " + balance);
     }
 }
